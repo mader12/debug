2b802d333be9183b3cec10fb5a0eda499ff44c15
mad <mader12@yandex.ru>
2018-07-28 15:36:48 +0300

    add

---
+++ src/D.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mad
+ * Date: 28.07.18
+ * Time: 15:28
+ */
+
+namespace D;
+
+
+class D
+{
+	public function __construct($var)
+	{
+		self::dumpExit($var);
+	}
+	
+	public static function dump($var)
+	{
+		echo '<pre style="background-color:#222; color: darkcyan;">';
+		var_dump($var);
+		echo '</pre>';
+		exit;
+	}
+	
+	public static function v($var)
+	{
+		echo '<pre style="background-color:#222; color: darkcyan;">';
+		var_dump($var);
+		echo '</pre>';
+	}
+}
